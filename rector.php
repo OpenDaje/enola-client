26d991f47d24ce8c6e8ee7de88cea908847b406b
zero <teclaizerai@gmail.com>
2024-12-10 11:16:47 +0100

    Fix rector issues.

--- rector.php
+++ rector.php
@@ -2,13 +2,20 @@
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\PHPUnit100\Rector\MethodCall\RemoveSetMethodsMethodCallRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->skip([
+        RenameMethodRector::class => [__DIR__ . '/tests/Api/ApiTestCase.php'],
+        RemoveSetMethodsMethodCallRector::class => [__DIR__ . '/tests/Api/ApiTestCase.php'],
     ]);
 
     // register a single rule
@@ -18,6 +25,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_74,
 
-        PHPUnitSetList::PHPUNIT_91,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 };
